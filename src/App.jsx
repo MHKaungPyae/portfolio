396dd26bdc822235860b78902ca21bd7106b0f3a
MHKaungPyae <6631503067@lamduan.mfu.ac.th>
2026-07-03 02:04:33 +0700

    Add SocialConnect component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import SideSocial from './components/SideSocial'
+import { SocialConnect } from '@/components/ui/connect-with-us'
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <About />
         <Skills />
         <Projects />
+        <SocialConnect />
         <Contact />
       </main>
       <Footer />
